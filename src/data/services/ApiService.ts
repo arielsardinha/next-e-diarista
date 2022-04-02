cf7d5a8e7fb46c618a56c70f12a72e62c6ab301d
arielsardinha <odararepresentacoes@outlook.com.br>
2022-04-02 19:49:04 -0300

    tratamento de token em requisições

--- src/data/services/ApiService.ts
+++ src/data/services/ApiService.ts
@@ -1,5 +1,6 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import { ApiLinksInterface } from 'data/@types/ApiLinksInterface';
+import { LocalStorage } from './StorageService';
 
 const url = process.env.NEXT_PUBLIC_API;
 
@@ -9,6 +10,46 @@ export const ApiService = axios.create({
         'Content-Type': 'application/json',
     },
 });
+
+ApiService.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (
+            error.response.status === 401 &&
+            error.response.data.code === 'token_not_valid'
+        ) {
+            handleTokenRefresh(error);
+        }
+        return Promise.reject(error);
+    }
+);
+
+async function handleTokenRefresh(error: { config: AxiosRequestConfig }) {
+    const tokenRefresh = LocalStorage.get<string>('token_refresh', '');
+
+    if (tokenRefresh) {
+        LocalStorage.clear('token');
+        LocalStorage.clear('token_refresh');
+        try {
+            const { data } = await ApiService.post('/auth/token/refresh/', {
+                refresh: tokenRefresh,
+            });
+            LocalStorage.set('token', data.acess);
+            LocalStorage.set('token_refresh', data.refresh);
+
+            ApiService.defaults.headers.Authorization = `Bearer ${data.acess}`;
+
+            error.config.headers.Authorization =
+                ApiService.defaults.headers.Authorization;
+
+            return ApiService(error.config);
+        } catch (err) {
+            return error;
+        }
+    } else {
+        return error;
+    }
+}
 
 export function linksResolver(
     Links: ApiLinksInterface[] = [],
