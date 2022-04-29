abed98d095083300942ad0a6e1a32c3b9eafe364
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2022-04-29 12:05:28 -0300

    montagem do formulário reutilizável para seleção de cidades

--- src/ui/components/inputs/UserForm/forms/CitiesForm.tsx
+++ src/ui/components/inputs/UserForm/forms/CitiesForm.tsx
@@ -1,13 +1,61 @@
+import {
+    Autocomplete,
+    CircularProgress,
+    TextField,
+    Typography,
+} from '@material-ui/core';
+import { CidadeInterface } from 'data/@types/EnderecoInterface';
 import useCitiesForm from 'data/hooks/components/inputs/UserForm/forms/useCitiesForm';
+import ChipField from 'ui/components/data-display/ChipField/ChipField';
 import { CitiesSelection } from '../UserForm.style';
 
 export const CitiesForm: React.FC<{ estado: string }> = ({ estado }) => {
-    const {
-        options,
-        handleNewCity,
-        handleDelete,
-        citiesList: listaCidades,
-        citiesName,
-    } = useCitiesForm(estado);
-    return <CitiesSelection></CitiesSelection>;
+    const { options, handleNewCity, handleDelete, citiesList, citiesName } =
+        useCitiesForm(estado);
+    return (
+        <CitiesSelection>
+            <Autocomplete
+                value={{ cidade: '' } as CidadeInterface}
+                onChange={(_ev, newValue) =>
+                    newValue && handleNewCity(newValue.cidade)
+                }
+                disablePortal
+                options={options}
+                getOptionLabel={(option) => option.cidade}
+                loading={citiesList.length === 0}
+                loadingText={'Carregando cidades...'}
+                style={{ gridArea: 'busca-cidade' }}
+                noOptionsText={'Nenhuma cidade com esse nome'}
+                renderInput={({ InputProps, ...params }) => (
+                    <TextField
+                        label={'Busque pelo nome da cidade'}
+                        {...params}
+                        inputProps={{
+                            ...InputProps,
+                            endAdornment: (
+                                <>
+                                    {citiesList.length ? (
+                                        <i className={'twf-search'} />
+                                    ) : (
+                                        <CircularProgress
+                                            size={20}
+                                            color={'inherit'}
+                                        />
+                                    )}
+                                    {InputProps.endAdornment}
+                                </>
+                            ),
+                        }}
+                        required={false}
+                    />
+                )}
+            />
+            <Typography>Cidades selecionadas</Typography>
+            <ChipField
+                itemsList={citiesName}
+                onDelete={handleDelete}
+                emptyMessage={'Nenhuma cidade selecionada ainda'}
+            />
+        </CitiesSelection>
+    );
 };
